5c5101d2f16eeb15a215b5ba79e0f5d70e4a9834
Tyler Warren <tylerdwarren98@gmail.com>
2016-08-04 15:10:23 -0700

    Finished all lists

--- app/src/main/java/tyfighter98/nmsnamegenorator/MainActivity.java
+++ app/src/main/java/tyfighter98/nmsnamegenorator/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Ensures planetName hasn't been initialized by something else
         plantName = "";
 
-//      <<---------Biome--------->>
+        //      <<---------Biome--------->>
         EditText biomeText = (EditText)findViewById(R.id.biomeText);
         String biome = biomeText.getText().toString();
 
@@ -75,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
                 plantName="Invalid Biome";
                 break;
         }
+
+        //      <<---------Resources--------->>
         EditText resourcesText = (EditText)findViewById(R.id.resourcesText);
         String resources = resourcesText.getText().toString();
 
@@ -141,6 +143,127 @@ public class MainActivity extends AppCompatActivity {
             default:
                 plantName="Invalid Resource";
         }
+
+        //      <<---------Life Forms--------->>
+        EditText lifeText = (EditText)findViewById(R.id.lifeFormsText);
+        String life = lifeText.getText().toString();
+
+        switch (life) {
+            case "All/Multiple":
+            case "All":
+            case "Multiple":
+                plantName+="a";
+                break;
+            case "Plants":
+            case "Plant":
+                plantName+="e";
+                break;
+            case "Creatures":
+            case "Creature":
+                plantName+="i";
+                break;
+            case "NPCs":
+            case "NPCS":
+            case "NPC":
+                plantName+="o";
+                break;
+            case "Uninhabited":
+            case "Nothing":
+            case "None":
+                plantName+="u";
+                break;
+            default:
+                plantName="Invalid Life Form";
+        }
+
+        //      <<---------Law--------->>
+        EditText lawText = (EditText)findViewById(R.id.lawText);
+        String law = lawText.getText().toString();
+
+        switch (law) {
+            case "Sentinels":
+            case "Sentinel":
+                plantName+="s";
+                break;
+            case "Lawless":
+            case "Nothing":
+            case "None":
+                plantName+="l";
+                break;
+            default:
+                plantName="Invalid Law Type";
+        }
+
+        //      <<---------Structures--------->>
+        EditText structuresText = (EditText)findViewById(R.id.structuresText);
+        String structures = structuresText.getText().toString();
+
+        switch (structures) {
+            case "Buildings":
+                plantName+="bu";
+                break;
+            case "Monoliths":
+                plantName+="mo";
+                break;
+            case "Portals":
+                plantName+="po";
+                break;
+            case "Monoliths/Portals":
+                plantName+="ma";
+                break;
+            case "Monoliths/Buildings":
+                plantName+="mu";
+                break;
+            case "Portals/Buildings":
+                plantName+="pu";
+                break;
+            case "All":
+            case "All Structures":
+            case "Structures":
+                plantName+="sa";
+                break;
+            case "No Structures":
+            case "None":
+            case "Nothing":
+                plantName+="no";
+                break;
+            default:
+                plantName="Invalid Structure";
+        }
+
+        //      <<---------Climate--------->>
+        EditText climateText = (EditText)findViewById(R.id.climateText);
+        String climate = climateText.getText().toString();
+
+        switch (climate) {
+            case "Cold":
+                plantName+="co";
+                break;
+            case "Melt/Heat":
+            case "Melt":
+            case "Heat":
+                plantName+="me";
+                break;
+            case "Safe":
+            case "Mild":
+            case "Normal":
+                plantName+="sa";
+                break;
+            case "Radiation":
+            case "Radioactive":
+                plantName+="ra";
+                break;
+            case "Toxic":
+                plantName+="to";
+                break;
+            default:
+                plantName="Invalid Climate";
+        }
+
+        //      <<---------Direction/Date--------->>
+        EditText directionText = (EditText)findViewById(R.id.directionText);
+        String direction = directionText.getText().toString();
+            plantName+=" " + direction;
     }
 }
 
