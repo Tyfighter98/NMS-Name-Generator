4ef2dced2a389c316b70e8ad6e6968b1f8bc11d6
Tyler Warren <tylerdwarren98@gmail.com>
2016-08-03 22:14:58 -0700

    Added lists for Biomes and Resources

--- app/src/main/java/tyfighter98/nmsnamegenorator/MainActivity.java
+++ app/src/main/java/tyfighter98/nmsnamegenorator/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Ensures planetName hasn't been initialized by something else
         plantName = "";
 
+//      <<---------Biome--------->>
         EditText biomeText = (EditText)findViewById(R.id.biomeText);
         String biome = biomeText.getText().toString();
 
@@ -38,8 +39,37 @@ public class MainActivity extends AppCompatActivity {
             case "Desert":
                 plantName+="De";
                 break;
+            case "Featureless":
+                plantName+="Fe";
+                break;
+            case "Floating Islands":
+                plantName+="Fi";
+                break;
+            case "Grassy":
+            case "Plains":
+                plantName+="Ga";
+                break;
             case "Lava":
                 plantName+="La";
+                break;
+            case "Liquid":
+                plantName+="Li";
+                break;
+            case "Mountains":
+                plantName+="Mo";
+                break;
+            case "Pillars":
+                plantName+="Pi";
+                break;
+            case "Rocky":
+                plantName+="Ro";
+                break;
+            case "Snow":
+            case "Ice":
+                plantName+="Si";
+                break;
+            case "Weird":
+                plantName+="We";
                 break;
             default:
                 plantName="Invalid Biome";
@@ -54,6 +84,59 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case "Suit":
                 plantName+="sut";
+                break;
+            case "Multi-Tool":
+            case "Tool":
+                plantName+="mul";
+                break;
+            case "Blueprint":
+                plantName+="bup";
+                break;
+            case "Trading":
+            case "Trade":
+                plantName+="tar";
+                break;
+            case "Ship/Suit":
+                plantName+="shs";
+                break;
+            case "Ship/Multi-Tool":
+            case "Ship/Tool":
+                plantName+="shm";
+                break;
+            case "Ship/Trading":
+            case "Ship/Trade":
+                plantName+="sht";
+                break;
+            case "Ship/Blueprint":
+                plantName+="shb";
+                break;
+            case "Suit/Multi-Tool":
+            case "Suit/Tool":
+                plantName+="sum";
+                break;
+            case "Suit/Trading":
+            case "Suit/Trade":
+                plantName+="str";
+                break;
+            case "Suit/Blueprint":
+                plantName+="sub";
+                break;
+            case "Multi-Tool/Trading":
+            case "Tool/Trading":
+            case "Multi-Tool/Trade":
+            case "Tool/Trade":
+                plantName+="mtr";
+                break;
+            case "Multi-Tool/Blueprint":
+            case "Tool/Blueprint":
+                plantName+="mbl";
+                break;
+            case "Blueprint/Trading":
+            case "Blueprint/Trade":
+                plantName+="btr";
+                break;
+            case "No Blueprint":
+                plantName+="nth";
                 break;
             default:
                 plantName="Invalid Resource";
